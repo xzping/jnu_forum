3a99cbddfacd3a4239dc62163657366146fc8359
xzping <849777710@qq.com>
2018-02-08 12:26:26 +0800

    footerController.class

--- src/main/java/com/xzp/forum/controller/FooterController.java
+++ src/main/java/com/xzp/forum/controller/FooterController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  */
 @Controller
-public class Footer {
+public class FooterController {
 	
 	@RequestMapping(path = "/aboutUs")
 	public String aboutUs() {
