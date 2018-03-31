9c94eb65769696d636f262de64684ce4fc3e61b5
xzping <849777710@qq.com>
2018-03-31 16:40:53 +0800

    撤销分页处理，延后开发，重构代码

--- src/main/java/com/xzp/forum/controller/TopicsController.java
+++ src/main/java/com/xzp/forum/controller/TopicsController.java
@@ -1,31 +1,24 @@
 package com.xzp.forum.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
-
 import com.xzp.forum.dao.AnswerDao;
 import com.xzp.forum.dao.MessageDao;
 import com.xzp.forum.dao.TopicDao;
 import com.xzp.forum.dao.UserDao;
-import com.xzp.forum.model.Answer;
 import com.xzp.forum.model.PageBean;
 import com.xzp.forum.model.Topic;
 import com.xzp.forum.model.User;
 import com.xzp.forum.service.PageService;
 import com.xzp.forum.util.HostHolder;
-import com.xzp.forum.util.ViewObject;
 
 @Controller
 public class TopicsController {
@@ -49,10 +42,10 @@ public class TopicsController {
 
 	@RequestMapping(path = "/topics", method = RequestMethod.GET)
 	public String displayAllTopics(Model model,HttpServletRequest request) {
-//		List<Topic> topics = topicDao.findAll();
+		List<Topic> topics = topicDao.findAll();
 		// 实现分页处理
-		PageBean<Topic> pageTopic=pageService.findItemByPage(1, 5);
-		List<Topic> topics=pageTopic.getItems();
+//		PageBean<Topic> pageTopic=pageService.findItemByPage(1, 5);
+//		List<Topic> topics=pageTopic.getItems();
 		String header = setHeader("all");
 		
 		User user=localHost.getUser();
@@ -63,32 +56,32 @@ public class TopicsController {
 		model.addAttribute("header", header);
 		model.addAttribute("answerDao", answerDao);
 		model.addAttribute("userDao", userDao);
-		model.addAttribute("currentPage", pageTopic.getCurrentPage());
-		model.addAttribute("hasMore", pageTopic.getIsMore());
+//		model.addAttribute("currentPage", pageTopic.getCurrentPage());
+//		model.addAttribute("hasMore", pageTopic.getIsMore());
 		return "topics";
 	}
 	
-	@RequestMapping(path = "/topics/page/{currentPage}", method = RequestMethod.GET)
-	public String displayAllTopicsPage(@PathVariable int currentPage, Model model,HttpServletRequest request) {
-//		List<Topic> topics = topicDao.findAll();
-		// 实现分页处理
-		PageBean<Topic> pageTopic=pageService.findItemByPage(currentPage, 5);
-		List<Topic> topics=pageTopic.getItems();
-		String header = setHeader("all");
-		
-		User user=localHost.getUser();
-		model.addAttribute("localHost", user.getUsername());
-		model.addAttribute("user", user);
-		model.addAttribute("newMessage", messageDao.countMessageByToId(user.getId()));
-		model.addAttribute("topics", topics);
-		model.addAttribute("header", header);
-		model.addAttribute("answerDao", answerDao);
-		model.addAttribute("userDao", userDao);
-		model.addAttribute("currentPage", currentPage);
-		model.addAttribute("hasMore", pageTopic.getIsMore());
-		
-		return "topics";
-	}
+//	@RequestMapping(path = "/topics/page/{currentPage}", method = RequestMethod.GET)
+//	public String displayAllTopicsPage(@PathVariable int currentPage, Model model,HttpServletRequest request) {
+////		List<Topic> topics = topicDao.findAll();
+//		// 实现分页处理
+//		PageBean<Topic> pageTopic=pageService.findItemByPage(currentPage, 5);
+//		List<Topic> topics=pageTopic.getItems();
+//		String header = setHeader("all");
+//		
+//		User user=localHost.getUser();
+//		model.addAttribute("localHost", user.getUsername());
+//		model.addAttribute("user", user);
+//		model.addAttribute("newMessage", messageDao.countMessageByToId(user.getId()));
+//		model.addAttribute("topics", topics);
+//		model.addAttribute("header", header);
+//		model.addAttribute("answerDao", answerDao);
+//		model.addAttribute("userDao", userDao);
+//		model.addAttribute("currentPage", currentPage);
+//		model.addAttribute("hasMore", pageTopic.getIsMore());
+//		
+//		return "topics";
+//	}
 	
 	@RequestMapping(path = "/topics/{category}", method = RequestMethod.GET)
 	public String displayTopicsByCategory(@PathVariable String category, Model model) {
